506f12aa41c5f92fc146f2ddffa673e70a237301
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-05-09 18:13:06 -0300

    separating server port to env file and adding new test to check email validation

--- tests/integration/ong.spec.js
+++ tests/integration/ong.spec.js
@@ -25,5 +25,22 @@ describe('ONG', () => {
 
         expect(response.body).toHaveProperty('id');
         expect(response.body.id).toHaveLength(8);
-    })
+    });
+
+    it('should be return a exception of invalid email', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: "Teste",
+                email: "testegmail.com",
+                whatsapp: "0 0000 0000",
+                city: "Teste",
+                uf: "SP"
+            });
+
+        expect(response.body).toHaveProperty('error')
+        expect(response.body).toHaveProperty('statusCode');
+        expect(response.body.statusCode).toBe(400);
+        expect(response.body.message).toBe("\"email\" must be a valid email");
+    });
 })
