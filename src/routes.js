8f611ed5599156f404ce9b031fc72e477a942993
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-05-09 02:21:04 -0300

    adding more packages, finishing screens and adding api request to get data of backend

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Detail from './Detail';
-import Incidents from './Incidents';
+import Detail from './pages/Detail';
+import Incidents from './pages/Incidents';
 
 const AppStack = createStackNavigator();
 
